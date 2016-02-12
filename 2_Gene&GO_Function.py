912cebb3bb646b556b98578db23da6ef2cc6d19e
NickJDnid16 <nid16@aber.ac.uk>
2016-02-12 21:45:57 +0000

    Fixing

---
+++ 2_Gene&GO_Function.py
@@ -0,0 +1,121 @@
+__author__ = 'nid16'
+
+import codecs
+import csv
+import sys
+data = {}
+import pprint
+outputfile = open('./Gene&GO_F.txt', mode='w')
+GOoutputfile = open('./Gene_With_Only_GO.txt', mode='w')
+FUNCoutputfile = open('./Gene_With_GO_FUNC .txt', mode='w')
+Seen =[]
+FUNC = []
+for line in open('./gene_association.fb'):
+
+    if(line[:2] == "FB"):#FlyBase = FB
+        split_string = line.split("\t")
+        genome = split_string [2]
+        GO = split_string [4]
+        dataMarker = split_string [6]
+        data[genome] = data.get(genome,"")+GO+","+dataMarker+","
+        if genome not in Seen:
+            Seen.append(genome)
+            GOoutputfile.write(genome + "\n")
+        FUNC.append(genome + "\t" + GO  + "\n")
+for line in open('./Single_Lethality_Genes.txt', mode='r'):
+    line = line.rstrip()
+    split_line = line.split(",")
+    gene = split_line[0]
+    data[gene] = data.get(gene,"")+str(split_line[1])
+
+
+################################################################
+
+# newFUNC = []
+#
+# geneSeen = []
+# for line in open('./Single_Lethality_Genes.txt', mode='r'):
+#     line = line.rstrip()
+#     split_line = line.split(",")
+#     gene = split_line[0]
+#     lethality = split_line[1]
+#     if (lethality == "lethal"):
+#         for line in FUNC:
+#             tempFUNC = []
+#             if gene in line and line not in geneSeen:
+#                 geneSeen.append(line)
+#                 line = line.strip()
+#                 tempFUNC.append(str(line) + "\t1")
+#                 print tempFUNC
+#                 newFUNC.append(tempFUNC)
+#     if (lethality == "viable"):
+#         for line in FUNC:
+#             tempFUNC = []
+#             if gene in line and line not in geneSeen:
+#                 geneSeen.append(line)
+#                 line = line.replace('\n','')
+#                 tempFUNC.append(str(line) + "\t0")
+#
+#                 print tempFUNC
+#                 newFUNC.append(tempFUNC)
+#
+#         print "Something"
+#         print tempFUNC
+# # #FUNCoutputfile.write("\n".join(newFUNC))
+#
+#
+# for element in newFUNC:
+#     #FUNCoutputfile.writelines(str(element)+"\n")
+#     FUNCoutputfile.write(" ".join(element) + "\n")
+
+###############################################################
+
+
+
+########################################################
+
+for x in data:
+    print (x,data[x])
+    outputfile.write(x+","+data[x]+"\n")
+  #  FUNCoutputfile.write()
+outputfile.close()
+
+
+########################################################
+
+
+inputfile = open('./Gene&GO_F.txt', mode='r')
+outputfile = open('./Gene&GO_F_With_Lethality.txt', mode='w')
+
+inputfile = csv.reader(inputfile, delimiter=',')
+
+previous = None
+
+
+writer = csv.writer(outputfile)
+# lethalwriter = csv.writer(lethaloutfile)
+# allwriter = csv.writer(alloutfile)
+
+for rows in inputfile:
+
+        if "viable" in str(rows[-1]) or "lethal" in str(rows[-1]):
+
+            if "GO" in str(rows):
+                writer.writerow(rows)
+                print rows
+########################################################
+
+outputfile.close()
+inputfile = open('./Gene&GO_F_With_Lethality.txt', mode='r')
+lethaloutfile = open('./Fly_Gene_Lethal.txt', mode='w')
+alloutfile = open('./Fly_Gene_Viable_Lethal.txt', mode='w')
+inputfile = csv.reader(inputfile, delimiter=',')
+for line in inputfile:
+    if "lethal" in line[-1]:
+        lethaloutfile.write(line[0] + "\n")
+    alloutfile.write(line[0] + "\n")
+
+
+
+
+
